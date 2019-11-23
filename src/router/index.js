bf3aeda3fc33bdb4f0daa3cd8476dc64167db31b
KevinZijlmans <kevin.zijlmans22@gmail.com>
2019-11-23 16:53:00 +0100

    Added children routes to admin page

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,10 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/pages/Home';
 import Cart from '@/pages/Cart';
-import Admin from '@/pages/Admin';
+import Index from '@/pages/admin/Index';
+import New from '@/pages/admin/New';
+import Products from '@/pages/admin/Products';
+import Edit from '@/pages/admin/Edit';
 
 Vue.use(Router);
 
@@ -14,14 +17,32 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/admin',
-      name: 'Admin',
-      component: Admin,
-    },
-    {
       path: '/cart',
       name: 'Cart',
       component: Cart,
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Index,
+
+      children: [
+        {
+          path: 'new',
+          name: 'New',
+          component: New,
+        },
+        {
+          path: '',
+          name: 'Products',
+          component: Products,
+        },
+        {
+          path: 'edit/:id',
+          name: 'Edit',
+          component: Edit,
+        },
+      ],
     },
   ],
 });
