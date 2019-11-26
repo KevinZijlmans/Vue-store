6c7c1f6ae1d5e8cf73b1f608a2c7050d8cf316ed
KevinZijlmans <kevin.zijlmans22@gmail.com>
2019-11-26 11:18:50 +0100

    Made new product form

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import * as VeeValidate from 'vee-validate';
 import App from './App';
 import router from './router';
+
+
+Vue.use(VeeValidate);
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
--- src/pages/admin/New.vue
+++ src/pages/admin/New.vue
@@ -3,5 +3,39 @@
     <div class="title">
       <h1>This is Admin/New</h1>
     </div>
-  </div>
+    <product-form 
+    @save-product="addProduct" 
+    :model="model" 
+    :manufacturers="manufacturers">
+  </product-form>
+  </div> 
 </template>
+
+<script>
+  import ProductFrom from '@/components/products/ProductForm.vue'
+  export default {
+    data () {
+      return {
+        model: {},
+        manufacturers: [
+          {
+            _id: 'sam',
+            name: 'Samsung',
+          },
+          {
+            _id: 'apple',
+            name: 'Apple',
+          },
+        ],
+      }
+    },
+    methods: {
+      addProduct (model) {
+        console.log('model', model)
+      }
+    },
+    components: {
+      'product-form': ProductFrom
+    }
+  }
+</script>
